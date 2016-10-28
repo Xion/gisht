ec83a472de1fcf3531a6715afb7cbc3ecc1a0f2c
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-10-28 12:12:59 -0400

    Tests for the Link: header parsing

--- src/ext/hyper.rs
+++ src/ext/hyper.rs
@@ -46,10 +46,15 @@ pub mod header {
             let mut links = HashMap::new();
             for value in raw {
                 let value = try!(from_utf8(value).map_err(|_| hyper::Error::Header));
+                if !RE.is_match(value) {
+                    return Err(hyper::Error::Header);
+                }
+                // TODO: split at comma and MATCH against the parts
                 for li_cap in RE.captures_iter(value) {
                     let li = LinkItem{rel: li_cap.name("rel").unwrap().to_owned(),
                                       url: li_cap.name("url").unwrap().to_owned()};
                     links.insert(li.rel.clone(), li);
+                    // TODO: consider erroring on duplicate rels
                 }
             }
             Ok(Link(links))
@@ -61,6 +66,46 @@ pub mod header {
             write!(fmt, "{}", self.0.values()
                 .map(|li| format!("<{}>; rel=\"{}\"", li.url, li.rel))
                 .collect::<Vec<_>>().join(", "))
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use hyper::header::Header;
+        use super::Link;
+
+        #[test]
+        fn link_parse_empty() {
+            let result = Link::parse_header(&[vec![]]);
+            assert!(result.is_err(), "Empty Link: unexpectedly parsed");
+        }
+
+        #[test]
+        fn link_parse_invalid() {
+            let input = "<http://example.com";
+            let result = Link::parse_header(&[input.as_bytes().to_vec()]);
+            assert!(result.is_err(),
+                "Link: header unexpectedly parsed: {}", input);
+        }
+
+        #[test]
+        fn link_parse_single() {
+            let url = "http://example.com";
+            let link = Link::parse_header(&[
+                format!(r#"<{}>; rel="next""#, url).as_bytes().to_vec()
+            ]).unwrap();
+            assert_eq!(url, link.0.get("next").unwrap().url);
+        }
+
+        #[test]
+        fn link_parse_nextprev() {
+            let next_url = "http://example.com/next";
+            let prev_url= "http://example.com/prev";
+            let link = Link::parse_header(&[format!(
+                r#"<{}>; rel="next", <{}>; rel="prev""#, next_url, prev_url
+            ).as_bytes().to_vec()]).unwrap();
+            assert_eq!(next_url, link.0.get("next").unwrap().url);
+            assert_eq!(prev_url, link.0.get("prev").unwrap().url);
         }
     }
 }
