64ef4123a336cd4c1d4611e4352e45809a6f86e4
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-11-03 15:40:27 -0700

    Only update GitHub gists every so often

--- src/hosts/github.rs
+++ src/hosts/github.rs
@@ -9,6 +9,7 @@ use std::fs;
 use std::io::{self, Read};
 use std::marker::PhantomData;
 use std::path::Path;
+use std::time::{Duration, SystemTime};
 
 use git2::{self, Repository};
 use hyper::client::{Client, Response};
@@ -50,7 +51,9 @@ impl Host for GitHub {
         let gist = try!(resolve_gist(gist));
 
         if gist.is_local() {
-            try!(update_gist(gist));
+            if needs_update(&gist) {
+                try!(update_gist(gist));
+            }
         } else {
             try!(clone_gist(gist));
         }
@@ -104,6 +107,11 @@ const API_URL: &'static str = "https://api.github.com";
 /// Size of the GitHub response page in items (e.g. gists).
 const RESPONSE_PAGE_SIZE: usize = 50;
 
+lazy_static! {
+    /// Minimum interval between updating (git-pulling) of gists.
+    static ref UPDATE_INTERVAL: Duration = Duration::from_secs(7 * 24 * 60 * 60);
+}
+
 
 /// Return a "resolved" Gist that has a GitHub ID associated with it.
 fn resolve_gist(gist: &Gist) -> io::Result<Cow<Gist>> {
@@ -145,6 +153,39 @@ fn id_from_binary_path<P: AsRef<Path>>(path: P) -> io::Result<String> {
 
 }
 
+
+/// Check whether given gist needs to be updated.
+///
+/// If the time since last update cannot be determined for whatever reason,
+/// the function will assume the update is necessary.
+fn needs_update<G: AsRef<Gist>>(gist: G) -> bool {
+    let gist = gist.as_ref();
+    let last = match last_update_time(&gist) {
+        Ok(time) => time,
+        Err(err) => {
+            warn!("Couldn't retrieve the last update time of gist {} ({}). \
+                   Assuming an update is needed.", gist.uri, err);
+            return true;
+        },
+    };
+
+    let now = SystemTime::now();
+    match now.duration_since(last) {
+        Ok(duration) => duration > *UPDATE_INTERVAL,
+        Err(err) => {
+            warn!("Last update time of gist {} is in the future ({}s from now). \
+                   Assuming an update is needed.", gist.uri, err.duration().as_secs());
+            true
+        },
+    }
+}
+
+/// Determine when was the last time a gist has been updated.
+fn last_update_time(gist: &Gist) -> io::Result<SystemTime> {
+    // Git writes .git/FETCH_HEAD at every pull, so just check its mtime.
+    let fetch_head = gist.path().join(".git").join("FETCH_HEAD");
+    fs::metadata(&fetch_head).and_then(|m| m.modified())
+}
 
 /// Update an already-downloaded gist.
 /// Since GitHub gists are Git repositories, this is basically a `git pull`.
