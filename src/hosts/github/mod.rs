694bfa616781bfb98aa3321c136ade8bbb6cf422
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-12-30 13:58:22 +0100

    Extract GitHub gist storage/update code to separate module

--- src/hosts/github/mod.rs
+++ src/hosts/github/mod.rs
@@ -5,24 +5,21 @@
 
 mod api;
 mod git;
+mod storage;
 mod util;
 
 
 use std::borrow::Cow;
-use std::fs;
 use std::io;
-use std::iter::Iterator;
 use std::marker::PhantomData;
 use std::path::Path;
-use std::time::{Duration, SystemTime};
 
-use git2;
 use regex::{self, Regex};
 use url::Url;
 
 use gist::{self, Datum, Gist};
-use util::{mark_executable, symlink_file};
 use super::{FetchMode, Host};
+use self::storage::{needs_update, update_gist, clone_gist};
 
 
 /// GitHub host ID.
@@ -155,6 +152,7 @@ impl Host for GitHub {
     }
 }
 
+
 /// Base URL to gist HTML pages.
 const HTML_URL: &'static str = "https://gist.github.com";
 
@@ -163,14 +161,6 @@ lazy_static! {
     static ref HTML_URL_RE: Regex = Regex::new(
         &format!("^{}/{}$", regex::quote(HTML_URL), r#"((?P<owner>[^/]+)/)?(?P<id>[0-9a-fA-F]+)"#)
     ).unwrap();
-}
-
-
-// Fetching gists
-
-lazy_static! {
-    /// Minimum interval between updating (git-pulling) of gists.
-    static ref UPDATE_INTERVAL: Duration = Duration::from_secs(7 * 24 * 60 * 60);
 }
 
 
@@ -217,136 +207,6 @@ fn id_from_binary_path<P: AsRef<Path>>(path: P) -> io::Result<String> {
         .and_then(|s| s.to_str()).map(String::from)
         .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound,
             format!("Invalid GitHub gist binary path: {}", path.display())))
-}
-
-
-/// Check whether given gist needs to be updated.
-///
-/// If the time since last update cannot be determined for whatever reason,
-/// the function will assume the update is necessary.
-fn needs_update<G: AsRef<Gist>>(gist: G) -> bool {
-    let gist = gist.as_ref();
-    trace!("Checking if GitHub gist {} requires an update...", gist.uri);
-
-    let last = match last_update_time(&gist) {
-        Ok(time) => time,
-        Err(err) => {
-            warn!("Couldn't retrieve the last update time of gist {} ({}). \
-                   Assuming an update is needed.", gist.uri, err);
-            return true;
-        },
-    };
-
-    let now = SystemTime::now();
-    match now.duration_since(last) {
-        Ok(duration) => duration > *UPDATE_INTERVAL,
-        Err(err) => {
-            warn!("Last update time of gist {} is in the future ({}s from now). \
-                   Assuming an update is needed.", gist.uri, err.duration().as_secs());
-            true
-        },
-    }
-}
-
-/// Determine when was the last time a gist has been updated.
-fn last_update_time(gist: &Gist) -> io::Result<SystemTime> {
-    // Git writes .git/FETCH_HEAD at every pull, so just check its mtime.
-    let fetch_head = gist.path().join(".git").join("FETCH_HEAD");
-    fs::metadata(&fetch_head).and_then(|m| m.modified())
-}
-
-/// Update an already-downloaded gist.
-/// Since GitHub gists are Git repositories, this is basically a `git pull`.
-fn update_gist<G: AsRef<Gist>>(gist: G) -> io::Result<()> {
-    let gist = gist.as_ref();
-    let path = gist.path();
-    assert!(gist.id.is_some(), "Gist {} has unknown GitHub ID!", gist.uri);
-    assert!(path.exists(), "Directory for gist {} doesn't exist!", gist.uri);
-
-    trace!("Updating GitHub gist {}...", gist.uri);
-    let reflog_msg = Some("gisht-update");
-    if let Err(err) = git::pull(&path, "origin", reflog_msg) {
-        match err.code() {
-            git2::ErrorCode::Conflict => {
-                warn!("Conflict occurred when updating gist {}, rolling back...", gist.uri);
-                try!(git::reset_merge(&path));
-                debug!("Conflicting update of gist {} successfully aborted", gist.uri);
-            },
-            git2::ErrorCode::Uncommitted => {
-                // This happens if the user has themselves modified the gist
-                // and their changes would be overwritten by the merge.
-                // There isn't much we can do in such a case,
-                // as it would lead to loss of user's modifications.
-                error!("Uncommitted changes found to local copy of gist {}", gist.uri);
-                return Err(git::to_io_error(err));
-            },
-            git2::ErrorCode::Unmerged => {
-                // This may happen if previous versions of the application
-                // (which didn't handle merge conflicts) has left a mess.
-                warn!("Previous unfinished Git merge prevented update of gist {}", gist.uri);
-                debug!("Attempting to rollback old Git merge of gist {}...", gist.uri);
-                try!(git::reset_merge(&path));
-                info!("Old Git merge of gist {} successfully aborted", gist.uri);
-            },
-            _ => return Err(git::to_io_error(err)),
-        }
-    }
-
-    debug!("GitHub gist {} successfully updated", gist.uri);
-    Ok(())
-}
-
-
-/// Clone the gist's repo into the proper directory (which must NOT exist).
-/// Given Gist object must have the GitHub ID associated with it.
-fn clone_gist<G: AsRef<Gist>>(gist: G) -> io::Result<()> {
-    let gist = gist.as_ref();
-    assert!(gist.uri.host_id == ID, "Gist {} is not a GitHub gist!", gist.uri);
-    assert!(gist.id.is_some(), "Gist {} has unknown GitHub ID!", gist.uri);
-    assert!(!gist.path().exists(), "Directory for gist {} already exists!", gist.uri);
-
-    // Check if the Gist has a clone URL already in its metadata.
-    // Otherwise, talk to GitHub to obtain the URL that we can clone the gist from
-    // as a Git repository.
-    let clone_url = match gist.info(Datum::RawUrl).clone() {
-        Some(url) => url,
-        None => {
-            trace!("Need to get clone URL from GitHub for gist {}", gist.uri);
-            let info = try!(api::get_gist_info(&gist.id.as_ref().unwrap()));
-            let url = match info.find("git_pull_url").and_then(|u| u.as_string()) {
-                Some(url) => url.to_owned(),
-                None => {
-                    error!("Gist info for {} doesn't contain git_pull_url", gist.uri);
-                    return Err(io::Error::new(io::ErrorKind::InvalidData,
-                        format!("Couldn't retrieve git_pull_url for gist {}", gist.uri)));
-                },
-            };
-            trace!("GitHub gist #{} has a git_pull_url=\"{}\"",
-                gist.id.as_ref().unwrap(), url);
-            url
-        },
-    };
-
-    // Create the gist's directory and clone it as a Git repo there.
-    debug!("Cloning GitHub gist from {}", clone_url);
-    let path = gist.path();
-    try!(fs::create_dir_all(&path));
-    try!(git::clone(&clone_url, &path));
-
-    // Make sure the gist's executable is, in fact, executable.
-    let executable = gist.path().join(&gist.uri.name);
-    try!(mark_executable(&executable));
-    trace!("Marked gist file as executable: {}", executable.display());
-
-    // Symlink the main/binary file to the binary directory.
-    let binary = gist.binary_path();
-    if !binary.exists() {
-        try!(fs::create_dir_all(binary.parent().unwrap()));
-        try!(symlink_file(&executable, &binary));
-        trace!("Created symlink to gist executable: {}", binary.display());
-    }
-
-    Ok(())
 }
 
 
