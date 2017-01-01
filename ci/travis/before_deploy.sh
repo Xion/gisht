4f24d7ee42b3373117f6843370d72c384b9bb975
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2017-01-01 13:53:33 +0100

    Try to install release deps on Travis

---
+++ ci/travis/before_deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# before_deploy: script for Travis
+
+
+_sudo=""
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    _sudo="sudo"
+fi
+
+
+#
+# Release dependencies
+#
+
+# Install Ruby.
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update >/dev/null
+    brew install ruby
+else
+    apt-get install ruby ruby-dev
+fi
+
+# Install fpm.
+_sudo gem install fpm
+
+
+#
+# Building release packages
+#
+
+# TODO: actually build the release packages
