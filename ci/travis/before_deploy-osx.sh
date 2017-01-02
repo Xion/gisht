d4a161a93d1b5424af4431d409e6c71f6e9a85c4
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2017-01-02 16:13:00 +0100

    Fix symlinking ssh_askpass on Travis/OSX

--- ci/travis/before_deploy-osx.sh
+++ ci/travis/before_deploy-osx.sh
@@ -5,9 +5,13 @@
 
 brew update >/dev/null
 
+
 # Install the replacement for the missing ssh-askpass
 # so that we can deploy using ad-hoc SSH key.
+SSH_ASKPASS=/usr/X11R6/bin/ssh-askpass
 brew tap theseal/ssh-askpass
 brew install ssh-askpass
-sudo mkdir -p /usr/X11R6/bin
-sudo ln -s /usr/local/bin/ssh-askpass /usr/X11R6/bin/ssh-askpass
+if [ ! -f "$SSH_ASKPASS" ]; then
+    sudo mkdir -p "$(dirname "$SSH_ASKPASS")"
+    sudo ln -s /usr/local/bin/ssh-askpass "$SSH_ASKPASS"
+fi
